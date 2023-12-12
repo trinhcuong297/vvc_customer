f02a621b22db6960a3af6935dec4e786d31fbb35
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-12 13:10:59 +0700

    init

--- models/billList.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-
-class billList(models.Model):
-    _name = 'customer.billList'
-    _description = "Customer Bill list"
-
-    name = fields.Char(string = "Tên")
-    
--- models/errorList.py
+++
@@ -1,6 +0,0 @@
-from odoo import api, fields, models
-
-
-class errorList(models.Model):
-    _name = 'customer.errorList'
-    _description = "Customer Error list"
