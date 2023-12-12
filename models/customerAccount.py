f872e1fe14a58b2a72a13c5caa341344c73d41f1
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-12 13:24:32 +0700

    init

--- models/customerAccount.py
+++ models/customerAccount.py
@@ -5,12 +5,12 @@ class CustomerAccount(models.Model):
     _name = 'customer.account'
     _description = "Customer Type"
 
-    # name = fields.Char(string = "Tên")
-    # phone = fields.Char(string = "Số điện thoại")
-    # homeLocation = fields.Char(string = "Vị trí căn hộ")
-    # project = fields.Char(string = "Dự án")
-    # archiveNeeded = fields.Integer(string = "Số hàng chưa nhận")
-    # billNeeded = fields.Integer(string = "Số hóa đơn chưa thanh toán")
-    # errorNum = fields.Integer(string = "Số lỗi báo cáo")
-    # email = fields.Char(string = "Email đăng nhập")
-    # password = fields.Char(string = "Mật khẩu")
+    name = fields.Char(string = "Tên")
+    phone = fields.Char(string = "Số điện thoại")
+    homeLocation = fields.Char(string = "Vị trí căn hộ")
+    project = fields.Char(string = "Dự án")
+    archiveNeeded = fields.Integer(string = "Số hàng chưa nhận")
+    billNeeded = fields.Integer(string = "Số hóa đơn chưa thanh toán")
+    errorNum = fields.Integer(string = "Số lỗi báo cáo")
+    email = fields.Char(string = "Email đăng nhập")
+    password = fields.Char(string = "Mật khẩu")
