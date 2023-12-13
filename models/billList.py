5d6a77558cc217c1decef21f56d11a0ef250da03
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-13 21:13:55 +0700

    init

--- models/billList.py
+++ models/billList.py
@@ -6,7 +6,7 @@ class ArchiveList(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Bill Type"
 
-    user_id = fields.Many2Many(
+    user_id = fields.Many2many(
         string='User',
         comodel_name='customer.account',
     )
