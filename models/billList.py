cf0b2ea7cb77785a1856b4d1f6837522c3b7e06e
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-14 15:50:57 +0700

    init

--- models/billList.py
+++ models/billList.py
@@ -10,8 +10,6 @@ class ArchiveList(models.Model):
         string='User',
         comodel_name='customer.account',
     )
-    homeLocation = fields.Char(string = "Vị trí căn hộ nhận")
-    project = fields.Many2one(string = "Dự án", comodel_name='project.project')
     type = fields.Char(string = "Tên hóa đơn")
     total = fields.Char(string = "Số tiền cần trả")
     status = fields.Selection([('store', 'Chưa thanh toán'),('sended','Đã thanh toán')],string = "Trạng thái")
