760dc8eed9df81521f3b4c0a47f0ef78c37c4acf
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-14 08:00:39 +0700

    init1

--- models/customerAccount.py
+++ models/customerAccount.py
@@ -10,8 +10,12 @@ class CustomerAccount(models.Model):
     phone = fields.Char(string = "Số điện thoại")
     homeLocation = fields.Char(string = "Vị trí căn hộ")
     project = fields.Many2one(string = "Dự án", comodel_name='project.project')
-    archiveNeeded = fields.Integer(string = "Số hàng chưa nhận")
+    archiveNeeded = fields.Integer(string = "Số hàng chưa nhận", compute='_compute_archive_needed' )
     billNeeded = fields.Integer(string = "Số hóa đơn chưa thanh toán")
     errorNum = fields.Integer(string = "Số lỗi báo cáo")
     email = fields.Char(string = "Email đăng nhập")
     password = fields.Char(string = "Mật khẩu")
+
+    def _compute_archive_needed(self):
+        for record in self:
+            record.archive_needed = self.env['customer.archive'].search_count(['user_id','=',record.id])
