1646e9133d3474d829f638647d846774d37faeeb
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-14 08:08:02 +0700

    init1

--- models/customerAccount.py
+++ models/customerAccount.py
@@ -18,4 +18,4 @@ class CustomerAccount(models.Model):
 
     def _compute_archive_needed(self):
         for record in self:
-            record.archive_needed = self.env['customer.archive'].search_count(['user_id','=',record.id])
+            record.archive_needed = self.env['customer.archive'].search_count(['user_id.id','=',record.id])
