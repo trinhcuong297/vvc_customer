06e2017d0c4aab9e70d637d10ae8efa18182b7da
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-16 23:54:43 +0700

    init

--- models/customerAccount.py
+++ models/customerAccount.py
@@ -18,10 +18,12 @@ class CustomerAccount(models.Model):
     isCompany = fields.Boolean(string='Khách hàng doanh nghiệp?')
     password = fields.Char(string = "Mật khẩu")
     archiveCount = fields.Integer(string='Đơn hàng chưa nhận',compute='_compute_archiveCount' )
+    archiveCount = fields.Integer(string='Đơn hàng chưa nhận',compute='_compute_archiveCount' )
+    archiveCount = fields.Integer(string='Đơn hàng chưa nhận',compute='_compute_archiveCount' )
         
     def _compute_archiveCount(self):
         for record in self:
-            record.archiveCount = self.env['customer.archive'].search_count([('user_id','=',record.id)])
+            record.archiveCount = self.env['customer.archive'].search_count([('user_id','=',record.id),('status','=','"store"')])
         
     
 
