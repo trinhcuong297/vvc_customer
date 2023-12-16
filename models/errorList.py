de5e65b9712435801eeabd2cd268623de41facf5
trinhcuong297 <trinhcuong297@gmail.com>
2023-12-16 23:07:40 +0700

    init

--- models/errorList.py
+++ models/errorList.py
@@ -7,9 +7,18 @@ class ArchiveList(models.Model):
     _description = "Error Type"
 
     user_id = fields.Many2one(
-        string='Người báo lỗi',
+        string='User',
         comodel_name='customer.account',
     )
+    name = fields.Char(string = "Tên người nhận", related='user_id.name')
+    phone = fields.Char(string = "Số điện thoại nhận", related='user_id.phone')
+    ground_ids = fields.Many2many(
+        string='Mã mặt bằng',
+        comodel_name='customer.ground',
+        related='user_id.ground_ids'
+    )
+    block = fields.Many2one(string = "Block", comodel_name='customer.block', related='user_id.block')
+    building = fields.Many2one(string = "Tòa nhà", comodel_name='customer.building', related='user_id.building')
     errLocation = fields.Char(string = "Vị trí báo lỗi")
     desc = fields.Char(string = "Mô tả")
     imgURL = fields.Char(string = "Link ảnh mô tả")
