b652139d6992cb1265c2b3d056692ab9e13b1c16
trinhcuong297 <trinhcuong297@gmail.com>
2024-01-04 13:41:13 +0700

    addQR

--- models/qrMonitor.py
+++ models/qrMonitor.py
@@ -7,4 +7,34 @@ class QrMonitor(models.Model):
 
     qrCodeScan = fields.Char(string='qrCodeScan')
     timeScan = fields.Datetime(string='Thời gian ghi', default=lambda self: fields.Datetime.to_string(datetime.datetime.now()) )
+    type = fields.Selection(
+        string='Loại',
+        selection=[('water', 'Chỉ số nước'), ('elec', 'Chỉ số điện')],
+        default = 'water'
+    )
+    ground_ids = fields.Many2one(
+        string='Mã mặt bằng',
+        comodel_name='customer.ground',
+    )
+    building = fields.Many2one(string = "Tòa nhà", comodel_name='customer.building',related='ground_ids.building')
+    block = fields.Many2one(string = "Block", comodel_name='customer.block',related='ground_ids.block')
+    recordImage = fields.Image(string = "Hình ảnh minh chứng")
+
+    @api.depends('qrCodeScan')
+    def getGround(self):
+        for rec in self:
+            allGround = self.env['customer.ground']
+            rec.ground_ids = allGround.search(['waterClock','=',rec.qrCodeScan])
+
+
+
+
+
+
+
+
+
+
+
+
     
