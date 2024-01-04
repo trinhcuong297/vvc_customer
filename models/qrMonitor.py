263f65026d3ed1932d636735aad88ede1918ec8e
trinhcuong297 <trinhcuong297@gmail.com>
2024-01-04 11:20:53 +0700

    addQR

--- models/qrMonitor.py
+++ models/qrMonitor.py
@@ -3,7 +3,8 @@ from odoo import api, fields, models
 
 class QrMonitor(models.Model):
     _name = 'customer.qr'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "QR monitor"
 
+    qrCodeScan = fields.Char(string='qrCodeScan')
+    
     
